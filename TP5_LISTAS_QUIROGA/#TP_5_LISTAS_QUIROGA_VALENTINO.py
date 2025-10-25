43d4796304b6206d6529289b437907ae29996fa6
ValentinoQuiroga4 <valentinoquirogabj@gmail.com>
2025-10-24 21:28:54 -0300

    AGREGO TP5_LISTAS QUIROGA

---
+++ TP5_LISTAS_QUIROGA/#TP_5_LISTAS_QUIROGA_VALENTINO.py
@@ -0,0 +1,57 @@
+#TP_5_LISTAS
+#QUIROGA VALENTINO
+
+#EJERCICIO_1
+multiplos_de_4 = list(range(4, 101, 4))
+print(multiplos_de_4)
+
+#EJERCICIO_2
+favoritos = ["pizza", "café", "juegos", "gato", "cine"]
+print(favoritos[-2])
+
+#EJERCICIO_3
+lista_vacia = []
+lista_vacia.append("sol")
+lista_vacia.append("montaña")
+lista_vacia.append("agua")
+print(lista_vacia)
+
+#EJERCICIO_4
+animales = ["perro", "gato", "vaca", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print(animales)
+
+#EJERCICIO_5
+#El programa primero crea una lista con 5 valores numéricos
+#Después elimina el número más grande de la lista
+#Por último muestra la lista final.
+
+#EJERCICIO_6
+numeros = list(range(10, 31, 5))
+print(numeros[:2])
+
+#EJERCICIO_7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "camioneta"
+autos[2] = "vento"
+print(autos)
+
+#EJERCICIO_8
+dobles = []
+dobles.append(5 * 2)
+dobles.append(10 * 2)
+dobles.append(15 * 2)
+print(dobles)
+
+#EJERCICIO_9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#EJERCICIO_10
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
+
